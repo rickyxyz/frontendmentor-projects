fba955c3a1967030e788404a2878abc2e128d1ff
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2025-12-28 00:01:41 +0700

    feat: add mobile navigation toggle animation

--- react/loopstudios-landing-page-main/src/components/Header.tsx
+++ react/loopstudios-landing-page-main/src/components/Header.tsx
@@ -1,30 +1,30 @@
 import { useState } from "react";
 import useIsMobile from "../Hooks/useIsMobile";
 
+const navItems = ["ABOUT", "CAREERS", "EVENTS", "PRODUCTS", "SUPPORT"];
+
 function Header() {
   const isMobile = useIsMobile();
   const [isOpen, setIsOpen] = useState(false);
 
   function toggleMenu() {
-    setIsOpen((p) => !p);
+    setIsOpen((p) => {
+      if (!p) document.body.style.overflow = "hidden";
+      else document.body.style.overflow = "unset";
+      return !p;
+    });
   }
 
   return (
     <header className="header">
       <img src="/images/logo.svg" alt="LoopStudios" className="header__logo" />
       <button className="header__menu_button" onClick={toggleMenu}>
-        <img
-          src="/images/icon-hamburger.svg"
-          alt="menu"
-          className={isOpen ? "hidden" : "visible"}
-        />
-        <img
-          src="/images/icon-close.svg"
-          alt="close"
-          className={!isOpen ? "hidden" : "visible"}
-        />
+        {!isOpen && <img src="/images/icon-hamburger.svg" alt="menu" />}
+        {isOpen && <img src="/images/icon-close.svg" alt="close" />}
       </button>
-      <nav
+
+      {/* Desktop navigation */}
+      {/* <nav
         className={
           isMobile ? "header__navigation-mobile" : "header__navigation"
         }
@@ -37,18 +37,18 @@ function Header() {
           <li>PRODUCTS</li>
           <li>SUPPORT</li>
         </ul>
-      </nav>
+      </nav> */}
 
       {/* Mobile navigation overlay */}
       <nav
-        className={`header__mobile_overlay ${!isOpen ? "hidden" : "visible"}`}
+        className={`header__mobile_overlay ${isOpen ? "visible" : "hidden"}`}
       >
         <ul>
-          <li>ABOUT</li>
-          <li>CAREERS</li>
-          <li>EVENTS</li>
-          <li>PRODUCTS</li>
-          <li>SUPPORT</li>
+          {navItems.map((item, index) => (
+            <li key={item} style={{ "--i": index + 1 } as React.CSSProperties}>
+              <a href="#">{item}</a>
+            </li>
+          ))}{" "}
         </ul>
       </nav>
     </header>
