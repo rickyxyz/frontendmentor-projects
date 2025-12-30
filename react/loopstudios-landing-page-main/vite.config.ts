189d1c12fc7bf80d6e00b0547759369b87a89127
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2025-12-30 16:39:12 +0700

    feat: add readme and build config

--- react/loopstudios-landing-page-main/vite.config.ts
+++ react/loopstudios-landing-page-main/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "https://rickyxyz.dev/frontendmentor-projects/loopstudios-landing-page-main",
+  build: {
+    outDir: "../../loopstudios-landing-page-main",
+  },
+});
